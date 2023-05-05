ca8b46e0a816f38bda84c3256ec424b871f5d039
okomarda <okomarda@yandex.ru>
2023-05-05 08:50:55 +0300

    Внесены изменения в файл channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,26 @@
+import json
+import os
 
+from googleapiclient.discovery import build
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel
+
+vdud = Channel("UCMCgOm8GZkHp8zJ6l7_hIuA")
+
+print(vdud.print_info())
