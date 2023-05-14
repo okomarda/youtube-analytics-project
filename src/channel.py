060e914c9e8581f681f1d0d6adcbd8769711ec40
okomarda <okomarda@yandex.ru>
2023-05-14 17:43:36 +0300

    Внесены изменения в файл channel.py

--- src/channel.py
+++ src/channel.py
@@ -47,20 +47,67 @@ class Channel:
         with open(youtube_file, 'w') as f:
             json.dump(json_data, f)
 
-#vdud = Channel()
-#print(vdud.print_info())
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    @classmethod
+    def __compare_subs(cls, other):
+        if not isinstance(other, (int, Channel)):
+            raise TypeError ("Операнд справа должен быть числом или объектом класса")
+        return other if isinstance(other, int) else int(other.subscriber)
+
+    def __add__(self, other):
+        subs = self.__compare_subs(other)
+        return int(self.subscriber) + int(subs)
+
+    def __sub__(self, other):
+
+        subs = self.__compare_subs(other)
+        return int(self.subscriber) - int(subs)
+
+    def __eq__(self, other):
+        subs = self.__compare_subs (other)
+        return int (self.subscriber) == int (subs)
+
+    def __gt__(self, other):
+        subs = self.__compare_subs (other)
+        return int (self.subscriber) > int (subs)
+
+    def __ge__(self, other):
+        subs = self.__compare_subs (other)
+        return int (self.subscriber) >= int (subs)
+
+    def __lt__(self, other):
+        subs = self.__compare_subs (other)
+        return int (self.subscriber) < int (subs)
+
+    def __le__(self, other):
+        subs = self.__compare_subs (other)
+        return int (self.subscriber) <= int (subs)
+
+#vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+#redactsiya = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
+#print(redactsiya.print_info())
 #print(vdud.title)
 #print(vdud.description)
 #print(vdud.url)
 #print(vdud.subscriber)
-#print(vdud.video)
-#print(vdud.view)
+#print(vdud.video_count)
+#print(vdud.view_count)
 #print(Channel.get_service())
 #print(vdud.channel_id)
 #print(vdud.channel_id)
 #vdud.channel_id = "sss"
 #vdud.to_json('vdud.json')
-
+#print(redactsiya.subscriber)
+#print(vdud + redactsiya)
+#print(vdud - redactsiya)
+#print(redactsiya - vdud)
+#print(vdud == redactsiya)
+#print(vdud > redactsiya)
+#print(vdud >= redactsiya)
+#print(vdud < redactsiya)
+#print(vdud <= redactsiya)
 
 
 
